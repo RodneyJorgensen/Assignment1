d77e43f08a78a2f67f70f2ce167df215f1d85edd
S <s>
2015-03-03 13:00:12 -0800

    {"type":"TI", "runs":"1"}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -24,6 +24,7 @@ public class CheckerboardKarel extends SuperKarel {
 			if (frontIsClear ()) {
 				putBeeper();
 				move();
+				move();
 				putBeeper();
 			}
 			
@@ -36,6 +37,7 @@ public class CheckerboardKarel extends SuperKarel {
 		while (frontIsClear()) {
 			if (frontIsClear ()) {
 				putBeeper();
+				move();
 				move();
 				putBeeper();
 			}
